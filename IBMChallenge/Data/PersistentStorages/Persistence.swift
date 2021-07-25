fd89910086051babb92ef80fdf27a0e08a21c656
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2021-07-25 02:29:25 -0400

    Initial Setup and Architecture Configuration

---
+++ IBMChallenge/Data/PersistentStorages/Persistence.swift
@@ -0,0 +1,12 @@
+//
+//  Persistence.swift
+//  IBMChallenge
+//
+//  Created by Gabriel de Oliveira Maciel on 25/07/21.
+//
+
+import CoreData
+
+struct PersistenceController {
+    // TODO: Setup Persistence Configurations
+}
